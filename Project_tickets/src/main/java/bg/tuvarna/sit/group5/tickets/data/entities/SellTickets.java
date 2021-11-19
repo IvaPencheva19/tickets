d5d478424aa4b3df5ef4c87c7b2db04c857b661d
IvaPencheva19 <IvaPencheva19@gmail.com>
2021-11-19 12:42:22 +0200

    Change entities

--- Project_tickets/src/main/java/bg/tuvarna/sit/group5/tickets/data/entities/SellTickets.java
+++ Project_tickets/src/main/java/bg/tuvarna/sit/group5/tickets/data/entities/SellTickets.java
@@ -13,6 +13,15 @@ public class SellTickets {
     @Basic
     @Column(name = "count")
     private Integer count;
+    @Basic
+    @Column(name = "seat")
+    private Integer seat;
+    @Basic
+    @Column(name = "custumer_name")
+    private String custumer_name;
+    @OneToOne
+    @JoinColumn(name="tickets")
+    private Tickets ticket_type;
 
     @ManyToOne
     @JoinColumn(name = "Distrib_event_idDistEvent", nullable = false)
@@ -44,16 +53,41 @@ public class SellTickets {
         this.distribEvent = distribEvent;
     }
 
+    public Integer getSeat() {
+        return seat;
+    }
+
+    public void setSeat(Integer seat) {
+        this.seat = seat;
+    }
+
+    public String getCustumer_name() {
+        return custumer_name;
+    }
+
+    public void setCustumer_name(String custumer_name) {
+        this.custumer_name = custumer_name;
+    }
+
+    public Tickets getTicket_type() {
+        return ticket_type;
+    }
+
+    public void setTicket_type(Tickets ticket_type) {
+        this.ticket_type = ticket_type;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         SellTickets that = (SellTickets) o;
-        return idSell == that.idSell && Objects.equals(count, that.count) && Objects.equals(distribEvent, that.distribEvent);
+        return idSell == that.idSell && Objects.equals(count, that.count) && Objects.equals(seat, that.seat) && Objects.equals(custumer_name, that.custumer_name) && Objects.equals(ticket_type, that.ticket_type) && Objects.equals(distribEvent, that.distribEvent);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(idSell, count, distribEvent);
+        return Objects.hash(idSell, count, seat, custumer_name, ticket_type, distribEvent);
     }
 }
+
